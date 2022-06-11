bf684a8788280a5c0f5867d205a500b0cf33d2b6
DenisLedovskiy <BigDen_07@bk.ru>
2022-06-11 16:07:57 +0300

    Initial Commit

---
+++ MKBTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MKBTest
+//
+//  Created by Денис Ледовский on 11.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
